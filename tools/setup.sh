cf4ea420078d4add56b8dfb1b418bafbb9b3efbe
root <root@ubuntu.(none)>
2013-02-01 06:10:54 -0800

    setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -3,8 +3,8 @@
 source settings
 source functions
 
-setup_develop $KEYSTONECLIENT_DIR 
-setup_develop $KEYSTONE_DIR 
+#setup_develop $KEYSTONECLIENT_DIR 
+#setup_develop $KEYSTONE_DIR 
 
 #setup_develop $GLANCECLIENT_DIR 
 #setup_develop $GLANCE_DIR 
@@ -12,7 +12,7 @@ setup_develop $KEYSTONE_DIR
 #setup_develop $CINDERCLIENT_DIR 
 #setup_develop $CINDER_DIR 
 #
-#setup_develop $QUANTUMCLIENT_DIR 
+setup_develop $QUANTUMCLIENT_DIR 
 #setup_develop $QUANTUM_DIR 
 #
 #setup_develop $NOVACLIENT_DIR 
