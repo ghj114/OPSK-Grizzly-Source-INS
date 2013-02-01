640b54a53ede659ee0c5111ba389d6d737f1280f
root <root@ubuntu.(none)>
2013-02-01 05:35:09 -0800

    new

---
+++ tools/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+source settings
+source functions
+
+setup_develop $KEYSTONECLIENT_DIR 
+setup_develop $KEYSTONE_DIR 
+
+#setup_develop $GLANCECLIENT_DIR 
+#setup_develop $GLANCE_DIR 
+#
+#setup_develop $CINDERCLIENT_DIR 
+#setup_develop $CINDER_DIR 
+#
+#setup_develop $QUANTUMCLIENT_DIR 
+#setup_develop $QUANTUM_DIR 
+#
+#setup_develop $NOVACLIENT_DIR 
+#setup_develop $NOVA_DIR 
+#
+#setup_develop $NOVNC_DIR 
+#
+#setup_develop $HORIZON_DIR 
