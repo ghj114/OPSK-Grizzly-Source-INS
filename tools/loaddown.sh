32b6403759814c5da8a7ae3244c07b0aeea760ba
root <root@node07.chenshake.com>
2013-03-28 15:03:48 +0800

    init

--- tools/loaddown.sh
+++ tools/loaddown.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-source settings
-source functions
+TOP_DIR=$(pwd)
+set -ex
+source $TOP_DIR/settings
+source $TOP_DIR/functions
 
 git_clone $KEYSTONECLIENT_REPO $KEYSTONECLIENT_DIR $KEYSTONECLIENT_BRANCH
 git_clone $KEYSTONE_REPO $KEYSTONE_DIR $KEYSTONE_BRANCH
